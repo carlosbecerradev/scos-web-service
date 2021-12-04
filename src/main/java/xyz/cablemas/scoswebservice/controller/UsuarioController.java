562b1fa3e30bb90db4d27603eb1b914b63487f30
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2021-12-04 12:54:54 -0500

    :hammer: Método web 'DELETE' para eliminar un Usuario por su Id ha sido agregado.

--- src/main/java/xyz/cablemas/scoswebservice/controller/UsuarioController.java
+++ src/main/java/xyz/cablemas/scoswebservice/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,12 @@ public class UsuarioController {
 			@PathVariable(name = "usuarioId") Long usuarioId) {
 		usuarioService.save(usuario);
 		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
+
+	@DeleteMapping
+	public ResponseEntity<HttpStatus> deleteUsuarioById(@PathVariable(name = "usuarioId") Long usuarioId) {
+		usuarioService.deleteById(usuarioId);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 }
