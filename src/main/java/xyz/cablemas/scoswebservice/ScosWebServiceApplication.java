20a6f0d6456b0b5559d121611bb8a6623efcf1cb
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2021-12-03 22:15:23 -0500

    :tada: Initial commit.

---
+++ src/main/java/xyz/cablemas/scoswebservice/ScosWebServiceApplication.java
@@ -0,0 +1,13 @@
+package xyz.cablemas.scoswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScosWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScosWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/cablemas/scoswebservice/ScosWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.cablemas.scoswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScosWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
