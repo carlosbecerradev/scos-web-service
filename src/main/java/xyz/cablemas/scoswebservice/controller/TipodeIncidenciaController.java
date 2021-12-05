1c78fb7024d1c086158c19feb594d09f1dd34204
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2021-12-05 01:42:15 -0500

    :hammer: API REST del módulo Tipo de Incidencia ha sido actualizada.

--- src/main/java/xyz/cablemas/scoswebservice/controller/TipodeIncidenciaController.java
+++ src/main/java/xyz/cablemas/scoswebservice/controller/TipodeIncidenciaController.java
@@ -1,5 +1,6 @@
 package xyz.cablemas.scoswebservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,26 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import xyz.cablemas.scoswebservice.entity.TipodeIncidencia;
-import xyz.cablemas.scoswebservice.entity.TipodeServicio;
 import xyz.cablemas.scoswebservice.service.TipodeIncidenciaService;
 
 @RestController
-@RequestMapping("/v1/tipodeincidencia")
+@RequestMapping("/v1/tipo-de-incidencia")
 public class TipodeIncidenciaController {
-	
+
+	@Autowired
 	private TipodeIncidenciaService tipodeincidenciaService;
 
-	
-	
-	@GetMapping("/listar")
-	public ResponseEntity<?> listar_GET() {
-		return new ResponseEntity<>(tipodeincidenciaService.findAll(),HttpStatus.OK);
+	@GetMapping
+	public ResponseEntity<?> getAll() {
+		return new ResponseEntity<>(tipodeincidenciaService.findAll(), HttpStatus.OK);
 	}
-	
-	
-	
-	@GetMapping("/buscar/{tipodeincidenciaId}")
-	public ResponseEntity<?> buscar(@PathVariable Long tipodeincidenciaId) {
+
+	@GetMapping("/{tipodeincidenciaId}")
+	public ResponseEntity<?> getOneById(@PathVariable Long tipodeincidenciaId) {
 		TipodeIncidencia tipodeincidencia = tipodeincidenciaService.findById(tipodeincidenciaId);
 
 		if (tipodeincidencia != null) {
@@ -39,53 +36,41 @@ public class TipodeIncidenciaController {
 		}
 
 		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
-}
-	
-	
-	
-	@PostMapping("/registrar")
-	public ResponseEntity<?> registrar(@RequestBody TipodeIncidencia tipodeincidencia)
-	{
+	}
+
+	@PostMapping
+	public ResponseEntity<?> insert(@RequestBody TipodeIncidencia tipodeincidencia) {
 		tipodeincidenciaService.insert(tipodeincidencia);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
-	
-	
-	
-	@PutMapping("/editar/{tipodeincidenciaId}")
-	public ResponseEntity<?> editar(@PathVariable Long tipodeincidenciaId,@RequestBody TipodeIncidencia tipodeincidencia)
-	{
-		TipodeIncidencia tipodeincidenciaDb=tipodeincidenciaService.findById(tipodeincidenciaId);
-		
-		if(tipodeincidenciaDb!=null)
-		{
+
+	@PutMapping("/{tipodeincidenciaId}")
+	public ResponseEntity<?> update(@PathVariable Long tipodeincidenciaId,
+			@RequestBody TipodeIncidencia tipodeincidencia) {
+		TipodeIncidencia tipodeincidenciaDb = tipodeincidenciaService.findById(tipodeincidenciaId);
+
+		if (tipodeincidenciaDb != null) {
 			tipodeincidenciaDb.setNombre(tipodeincidencia.getNombre());
 			tipodeincidenciaDb.setActivo(tipodeincidencia.getActivo());
 			tipodeincidenciaDb.setFechaDeCreacion(tipodeincidencia.getFechaDeCreacion());
-			
-			
-			
+
 			tipodeincidenciaService.update(tipodeincidenciaDb);
 			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
-		
-		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
-}
-	
-	
-	@DeleteMapping("/borrar/{tipodeincidenciaId}")
-	public ResponseEntity<?> borrar(@PathVariable Long tipodeincidenciaId)
-	{
-		TipodeIncidencia tipodeincidenciaDb=tipodeincidenciaService.findById(tipodeincidenciaId);
-		
-		if(tipodeincidenciaDb!=null) 
-		{
-			tipodeincidenciaService.delete(tipodeincidenciaId);
-			return new ResponseEntity<Void>(HttpStatus.OK);
-		}
-		
+
 		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 	}
 
-	
+	@DeleteMapping("/{tipodeincidenciaId}")
+	public ResponseEntity<?> deleteById(@PathVariable Long tipodeincidenciaId) {
+		TipodeIncidencia tipodeincidenciaDb = tipodeincidenciaService.findById(tipodeincidenciaId);
+
+		if (tipodeincidenciaDb != null) {
+			tipodeincidenciaService.delete(tipodeincidenciaId);
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+	}
+
 }
