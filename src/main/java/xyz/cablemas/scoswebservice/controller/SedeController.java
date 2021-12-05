0ea0eccefe7fc0d44c4157f278c185012dc5b4dd
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2021-12-05 01:37:53 -0500

    :hammer: API REST del módulo Sede ha sido actualizada.

--- src/main/java/xyz/cablemas/scoswebservice/controller/SedeController.java
+++ src/main/java/xyz/cablemas/scoswebservice/controller/SedeController.java
@@ -1,6 +1,5 @@
 package xyz.cablemas.scoswebservice.controller;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,38 +13,33 @@ import lombok.AllArgsConstructor;
 import xyz.cablemas.scoswebservice.entity.Sede;
 import xyz.cablemas.scoswebservice.service.SedeService;
 
-
 @RestController
 @RequestMapping("/v1/sede")
 @AllArgsConstructor
 public class SedeController {
-	
+
 	private final SedeService sedeService;
-	
-	
-	@GetMapping("/listar")
-	public ResponseEntity<?> listar_GET() {
-		return new ResponseEntity<>(sedeService.findAll(),HttpStatus.OK);
+
+	@GetMapping
+	public ResponseEntity<?> getAll() {
+		return new ResponseEntity<>(sedeService.findAll(), HttpStatus.OK);
 	}
-	
-	@GetMapping("/buscar/{sedeId}")
-	public ResponseEntity<?> buscar(@PathVariable Long sedeId) 
-	{
-		Sede sede=sedeService.findById(sedeId);
-		
-		if(sede!=null) {
-			return new ResponseEntity<>(sede,HttpStatus.OK);
+
+	@GetMapping("/{sedeId}")
+	public ResponseEntity<?> getOneById(@PathVariable Long sedeId) {
+		Sede sede = sedeService.findById(sedeId);
+
+		if (sede != null) {
+			return new ResponseEntity<>(sede, HttpStatus.OK);
 		}
-		
+
 		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 	}
-	
-	@PostMapping("/registrar")
-	public ResponseEntity<?> registrar(@RequestBody Sede sede)
-	{
+
+	@PostMapping
+	public ResponseEntity<?> insert(@RequestBody Sede sede) {
 		sedeService.insert(sede);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
-	
 
 }
